29717521f8eafc3bbd7bced869d05315df8a50b5
Bako2022 <88108918+Bako2022@users.noreply.github.com>
2021-08-12 20:12:16 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import Scrollbar from 'smooth-scrollbar';
+
+
+var options={
+    'damping':0.05,
+    'alwaysShowTracks':true
+
+}
+
+
+Scrollbar.init(document.querySelector('#my-scrollbar'),options);
